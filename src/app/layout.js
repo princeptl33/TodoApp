b7c09fcedb5daa64fce90c7ced91c4d072870025
Prince Tech <princepatel8321522@gmail.com>
2023-05-12 18:56:29 -1200

    update  header
    header update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 import './globals.css'
 import "../../styles/app.scss"
 import { Inter } from 'next/font/google'
-import Header from './header'
 import { ContextProvider } from './components/Clients'
+import Header from './Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
